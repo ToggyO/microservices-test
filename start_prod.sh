0f54bb27f97d26fdb5072fdc05985679f3c3df99
ToggyO <togushakov@magora-systems.com>
2020-07-26 14:57:28 +0700

    Add service's internal network & nginx external network

--- start_prod.sh
+++ start_prod.sh
@@ -3,7 +3,7 @@ export $(cat .env | xargs)
 docker-compose down
 docker rmi $(docker images "microservices-test*" -q)
 docker rmi $(docker images -f "dangling=true" -q)
-docker-compose up --build
+docker-compose up -d --build
 export $(cat clean.env | xargs)
 #export $(cat .env | xargs) && docker-compose -f ./docker-compose.local.yml up --build
 #while read line; do export "$line";
